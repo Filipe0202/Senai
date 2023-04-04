31427e285aa6eca98f8969a4d35a537505d49d84
Filipe Kreutzfeld <90661481+Filipe0202@users.noreply.github.com>
2023-04-04 18:50:36 -0300

    Add files via upload

--- Aula2903/src/aula2903/Exercicio_Triangulos_Java2.java
+++ Aula2903/src/aula2903/Exercicio_Triangulos_Java2.java
@@ -4,38 +4,31 @@ package aula2903;
 import javax.swing.JOptionPane;
 
 
-public class Exercicio_Triangulos_Java {
+public class Exercicio_Triangulos_Java2 {
     
     public static void main(String[] args) {
-        
-        int ladoA, ladoB, ladoC;
-        
-        ladoA = Integer.parseInt(JOptionPane.showInputDialog(" Informe o primeiro valor: "));
-        ladoB = Integer.parseInt(JOptionPane.showInputDialog(" Informe o segundo valor: "));
-        ladoC = Integer.parseInt(JOptionPane.showInputDialog(" Informe o terceiro valor: "));
-    
-        //SE 
-        if(((ladoA + ladoB) > ladoC) e ((ladoB + ladoC) > ladoA) e ((ladoA + ladoC) > ladoB)){
-			
-		
-	if((ladoA == ladoB) e (ladoB == ladoC));{
-            JOptionPane.showMessageDialog(null, "é um Equilatero");
-			
-	}else if((ladoA == ladoB) ou (ladoB == ladoC) ou (ladoA == ladoC));{
-            JOptionPane.showMessageDialog(null, "é um Isóceles");
-            
-	}else{
-            JOptionPane.showMessageDialog(null, "é um Escaleno");
-        }
-	}else{
-            JOptionPane.showMessageDialog(null, "Não é possível formar um triângulo com os valores informados");
-		
-        }	
+        float ladoA, ladoB, ladoC;
 
-     
-        
-       
-       
+        ladoA = Float.parseFloat(JOptionPane.showInputDialog(null, "Informe o lado A"));
+        ladoB = Float.parseFloat(JOptionPane.showInputDialog(null, "Informe o lado B"));
+        ladoC = Float.parseFloat(JOptionPane.showInputDialog(null, "Informe o lado C"));
+
+        if (((ladoA + ladoB) > ladoC) && ((ladoB + ladoC) > ladoA) && ((ladoA + ladoC) > ladoB)) {
+
+            if ((ladoA == ladoB) && (ladoB == ladoC)) {
+                JOptionPane.showMessageDialog(null, "Triângulo Equilátero");
+            } else {
+                if ((ladoA == ladoB) || (ladoB == ladoC) || (ladoA == ladoC)) {
+                    JOptionPane.showMessageDialog(null, "Triângulo Isósceles");
+                } else {
+                    JOptionPane.showMessageDialog(null, "Triângulo Escaleno");
+                }
+            }
+        } else {
+            JOptionPane.showMessageDialog(null, "Não é possível formar um triângulo com os valores informados");
+        }
+
     }
     
+   
 }
